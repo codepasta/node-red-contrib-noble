86d5d9ab11d2739e52e7ea98295603a3c59cd9ed
Ganesh Ramanathan <ganesh@datazug.ch>
2022-01-09 18:12:11 +0100

    Update node-red-contrib-noble.js

--- noble/node-red-contrib-noble.js
+++ noble/node-red-contrib-noble.js
@@ -23,7 +23,7 @@
 module.exports = function(RED) {
     "use strict";
 
-    var noble = require('noble');
+    var noble = require('@abandonware/noble');
     var os = require('os');
     
     // The main node definition - most things happen in here
